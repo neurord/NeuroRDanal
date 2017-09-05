02f2787ba43ee245130559d543def1d31a1276e9
Avrama Blackwell <avrama@gmu.edu>
2017-09-05 15:42:44 -0400

    make argument parsing standardized and create a few functions in preparation for merging various sig2*.py programs

--- sig2-fig.py
+++ sig2-fig.py
@@ -1,7 +1,14 @@
 #sig2-fig.py
-#Generate signature traces, plot them, and create files for plotting in othe software
-#ARGS="subdir/fileroot,LTPmol,LTDmol,tstart tend" where tstart and tend used to calculate basal value
-#ARGS="Model_SPNspineAChm4R_Gshydr5_GapD,Aphos CKpCam Epac1 Pkc,2ag,10 20"
+#Generate signature traces and plot them, or create files of time samples for analysis in other software
+#ARGS="subdir/fileroot,parameter,LTPmol,LTDmol,tstart tend,4 thresholds,sample times" where tstart and tend used to calculate basal value
+#ARGS="Model_SPNspineAChm4R_Gshydr5_GapD,,Aphos CKpCam Epac1 Pkc,2ag,10 20"
+#for file output:
+#ARGS="subdir/fileroot,parameter,LTPmol,LTDmol,tstart tend,sample durs,sample times,file_suffix"
+#ARGS="Model_SPNspineAChm4R_Gshydr5_GapD,,Aphos CKpCam Epac1 Pkc,2ag,10 20,30 60,60 120,ACEP2"
+#last 3 parameters and 2nd parameter is optional
+#############TODO: add in the other signatures, such as AUC and contiguous time above from sig2.py?  OR, add that as option?  Then replace sig2.py
+
+
 from __future__ import print_function
 from __future__ import division
 
@@ -12,8 +19,6 @@ import sys
 import glob
 from NeuroRDanal import h5utils
 from NeuroRDanal import plot_h5 as pu5
-from collections import OrderedDict
-import plot_h5 as pu5
 
 #specify either 'mean' or column numbers, or region names
 #These should probably be additional parameters/arguments
@@ -22,7 +27,7 @@ coltype=['nonspine','sa1[0]']#,'sa1[1]','sa1[2]','sa1[3]','sa1[4]','sa1[5]','sa1
 trials=3 #make trials=1 if coltype=='mean'
 normYN=1
 textsize=8
-plotYN=1
+print_measures=1
 
 try:
     args = ARGS.split(",")
@@ -49,7 +54,31 @@ ltp_molecules=args[2].split()
 ltd_molecules=args[3].split()
 tstart,tend=args[4].split()
 
-all_molecules=ltp_molecules+ltd_molecules
+#if provide file suffix, then files will be written, and plots will not be generated
+if len(args[7]):
+    fname_suffix=args[7]
+    plotYN=0
+    normYN=0
+else:
+    fname_suffix=''
+    plotYN=1
+
+if len(args[5]):
+    if plotYN:
+        thresh=args[5]
+    else:
+        time_thresh=args[5].split()
+else:
+    if plotYN:
+        thresh=[0,0,0,0]
+    else:
+        time_thresh=[60,30] #units are sec.
+if len(args[6]):
+    samp_times=args[6].split()
+else:
+    samp_times=[60,120]
+
+all_molecules=sorted(ltp_molecules+ltd_molecules)
 num_mols=len(all_molecules)
 
 #Read in all files and assemble relevant columns into large data matrix
@@ -57,12 +86,12 @@ all_peaks={}
 all_sig_array={}
 for molnum,mol in enumerate(all_molecules):
     for fnum,fname in enumerate(fname_roots):
-        temp=[]
         fnm=fname+'_'+mol+'plas.txt'
         f = open(fnm, 'r+')
         header=f.readline()
         f.close()
         head_names=header.split()
+        temp=[]
         if coltype=='mean':
             col_num=[head_names.index(x) for x in head_names if coltype in x][0:-1]
         elif type(coltype) is list and type(coltype[0]) is str:
@@ -71,6 +100,8 @@ for molnum,mol in enumerate(all_molecules):
             col_num=[val for sublist in temp for val in sublist]
         else:
             col_num=coltype
+        if molnum==0:
+            print ('fname:', fname, 'columns:', col_num)
         alldata=np.loadtxt(fnm,skiprows=1)
         time=alldata[:,0]
         data_cols=alldata[:,col_num]
@@ -85,7 +116,7 @@ for molnum,mol in enumerate(all_molecules):
             sig_array=np.zeros((len(fname_roots),len(time),len(col_num)))
             peaks=np.zeros((len(fname_roots),len(col_num)))
         #
-        ########################### normalize and create arrays of molecules ##########################
+        ########################### normalize (optional) and create arrays of molecules ##########################
         #
         if normYN:
             basal=np.mean(data_cols[pstrt+extra:pend+extra],axis=0)
@@ -101,43 +132,25 @@ for molnum,mol in enumerate(all_molecules):
     all_peaks[mol]=peaks
 
 ########################### Signature part ##########################
-#place into function  (or two)
-sig_ltp=np.zeros((len(fname_roots),len(time),len(col_num)))
-sig_ltd=np.zeros((len(fname_roots),len(time),len(col_num)))
-
-#for each file, calculate two signatures:
-#   (A): discrim1Const+discrim11*mol1trace+discrim12*mol2trace etc.
-#        apply to each trial and repeat for discrim2
-#        1st normalize by baseline subtraction (and baseline division for non-zero molecules)
-
-
-#   (B): sum normalized molecules separately for dend and spine, separately for LTP and LTD
-#        apply to each trial, 1st normalize by baseline subtraction (and peak factor)
-for molnum,mol in enumerate(all_molecules):
-    region_peak=np.max(all_peaks[mol],axis=0)
-    for reg in range(num_regions):
-        colset=[tr+reg*trials for tr in range(trials)]
-        region_peak[colset]=np.max(region_peak[colset])
-    print (mol,region_peak)
-    for fnum,fname in enumerate(fname_roots):
-        if mol in ltp_molecules:
-            sig_ltp[fnum]=sig_ltp[fnum]+all_sig_array[mol][fnum]/region_peak
-        else:
-            sig_ltd[fnum]=sig_ltd[fnum]+all_sig_array[mol][fnum]/region_peak
-#
-#show figures (and create output files for publication figures)
-figtitle=fnm[0:fnm.find('-')]
-label=[[] for p in parval]
-domain=[head_names[x].split(mol) for x in col_num]
-for par in range(len(parval)):
-    label[par]=[parval[par]+' '+dom[0][0:6]+dom[1] for dom in domain]
-sign_title=args[2]+' vs '+args[3]
 
 if plotYN:
-  if len(ltd_molecules):
-    pu5.plot_signature(label,sig_ltp,time,figtitle,sign_title,textsize,thresh,sig_ltd)
-  else:
-    pu5.plot_signature(label,sig_ltp,time,figtitle,sign_title,textsize,thresh)
-
-        
-    
+    #calculate signatures
+    sig_ltp,sig_ltd=h5utils.calc_sig(all_sig_array,all_peaks,all_molecules,ltp_molecules,trials,fname_roots,time,col_num)
+    #show figures (and create output files for publication figures?)
+    figtitle=fnm[0:fnm.find('-')]
+    label=[[] for p in parval]
+    domain=[head_names[x].split(mol) for x in col_num]
+    for par in range(len(parval)):
+        label[par]=[parval[par]+' '+dom[0][0:6]+dom[1] for dom in domain]
+    sign_title=args[2]+' vs '+args[3]
+    #
+    pyplot.ion()
+    if len(ltd_molecules):
+        pu5.plot_signature(label,sig_ltp,time,figtitle,sign_title,textsize,thresh,sig_ltd)
+    else:
+        pu5.plot_signature(label,sig_ltp,time,figtitle,sign_title,textsize,thresh)
+else:
+    #or create output files for analysis of signatures
+    h5utils.sig_outfile_multi(time_thresh,time,samp_times,pstrt,pend,fname_roots,fname_suffix,col_num,trials,all_sig_array)
+    #
+    #h5utils.sig_outfile_single(time_thresh,time,samp_times,pstart,pend,fname_roots,fname_suffix,col_num,trials,all_sig_array)
