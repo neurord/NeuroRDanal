e6d2f2fe29b5aae5f9ae5a4864a462efae496be0
Avrama Blackwell <avrama@gmu.edu>
2017-09-18 13:01:39 -0400

    create signatures for spatial profile of long dendrite and write output files

--- sig2-fig.py
+++ sig2-fig.py
@@ -20,10 +20,12 @@ import glob
 from NeuroRDanal import h5utils
 from NeuroRDanal import plot_h5 as pu5
 
-#specify either 'mean' or column numbers, or region names
+#specify either 'mean' or column numbers, or region names - must match sig.py output
 #These should probably be additional parameters/arguments
 #coltype=[1,2,3,4,5,6]
-coltype=['nonspine','sa1[0]']#,'sa1[1]','sa1[2]','sa1[3]','sa1[4]','sa1[5]','sa1[6]','sa1[7]','sa1[8]','sa1[9]']
+coltype=[str(x) for x in np.linspace(1.0,19.0,10)] #for spatial samples in long dendrite
+#coltype=['nonspine','sa1[0]','sa1[1]','sa1[6]']#'sa1[2]','sa1[3]','sa1[4]','sa1[5]','sa1[7]','sa1[8]','sa1[9]'
+suffix='dend'  #either dend to use spatial average files or plas to use spine outputs from sig.py  
 trials=3 #make trials=1 if coltype=='mean'
 normYN=1
 textsize=8
@@ -40,11 +42,12 @@ except NameError: #NameError refers to an undefined variable (in this case ARGS)
 
 #Identify set of filenames and parameter values
 if len(args[1]):
-    pattern=args[0]+'-'+args[1]
+    pattern=args[0]+'-'+'*-'.join(args[1].split())
 else:
     pattern=args[0]
-fnames = glob.glob(pattern+"*plas.txt")
-print(pattern+"*plas.txt, ", len(fnames), 'files found')
+globpattern=pattern+"*"+suffix+".txt"
+fnames = glob.glob(globpattern)
+print(globpattern, len(fnames), 'files found')
 fname_roots=sorted(set([fnm[0:fnm.rfind('_')] for fnm in fnames]))
 fname_endings=set([fnm[fnm.rfind('_'):] for fnm in fnames])
 parval=[fnm[fnm.find('-')+1:].replace('-',' ') for fnm in fname_roots]
@@ -86,7 +89,7 @@ all_peaks={}
 all_sig_array={}
 for molnum,mol in enumerate(all_molecules):
     for fnum,fname in enumerate(fname_roots):
-        fnm=fname+'_'+mol+'plas.txt'
+        fnm=fname+'_'+mol+suffix+'.txt'
         f = open(fnm, 'r+')
         header=f.readline()
         f.close()
@@ -96,7 +99,7 @@ for molnum,mol in enumerate(all_molecules):
             col_num=[head_names.index(x) for x in head_names if coltype in x][0:-1]
         elif type(coltype) is list and type(coltype[0]) is str:
             for col in coltype:
-                temp.append([head_names.index(x) for x in head_names if '_t' in x and col in x])
+                temp.append([head_names.index(x) for x in head_names if '_t' in x and x.startswith(col)])
             col_num=[val for sublist in temp for val in sublist]
         else:
             col_num=coltype
@@ -150,7 +153,27 @@ if plotYN:
     else:
         pu5.plot_signature(label,sig_ltp,time,figtitle,sign_title,textsize,thresh)
 else:
-    #or create output files for analysis of signatures
+    #or create output files for analysis of signatures. either 1 file with multiple points
     h5utils.sig_outfile_multi(time_thresh,time,samp_times,pstrt,pend,fname_roots,fname_suffix,col_num,trials,all_sig_array)
-    #
+    #or mulitple files, each with basal and 1 other point
     #h5utils.sig_outfile_single(time_thresh,time,samp_times,pstart,pend,fname_roots,fname_suffix,col_num,trials,all_sig_array)
+
+#To write the signature file traces:
+if 'sa1[' in coltype:
+    newdomain=[[dom[0].replace('sa1[','sp').replace(']',''),dom[1]] for dom in domain]
+else:
+    newdomain=domain
+newlabel=[''.join(newdomain[x]) for x in range(len(newdomain))]
+sig=sig_ltp-sig_ltd
+for ii,file_cond in enumerate(fname_roots):
+    for array,plastype,molecules in zip([sig_ltp,sig_ltd,sig],['ltp','ltd','sig'],[ltp_molecules,ltd_molecules,ltp_molecules+ltd_molecules]):
+        fname=file_cond+'_'+plastype+'.txt'
+        param_name='_'.join(file_cond.split('-')[1:]).replace('stim','')
+        header='time '+' '.join([plastype+'_'+param_name+labl for labl in newlabel])
+        f=open(fname, 'w')
+        f.write(' '.join(molecules)+'\n')
+        f.write(header+'\n')
+        np.savetxt(f,array[ii], fmt='%.4f', delimiter=' ')
+        f.close()
+        
+
